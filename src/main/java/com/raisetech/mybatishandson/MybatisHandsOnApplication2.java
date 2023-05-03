d0d97db6a9ba7df15bf87e2a4dc0d451efaf5a90
Natsuko <makumaku2014@gmail.com>
2023-05-03 19:01:13 +0900

    application.java削除前コミット

--- src/main/java/com/raisetech/mybatishandson/MybatisHandsOnApplication2.java
+++ src/main/java/com/raisetech/mybatishandson/MybatisHandsOnApplication2.java
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MybatisHandsOnApplication {
-	public static void main(String[] args) {
-	SpringApplication.run(MybatisHandsOnApplication.class, args);
-
-
-
-}
+public class MybatisHandsOnApplication2 {
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisHandsOnApplication.class, args);
+    }
 }
